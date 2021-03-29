29d0afea06ca34d1036a0782aacb1079bcbe0071
Peco Dory <pecodory@Pecos-MBP.attlocal.net>
2021-03-29 18:17:40 -0400

    mapped card images and rendered

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,36 +1,76 @@
 import React from "react";
+import axios from "axios";
+import "./Form.css";
 import { TextInput, Box } from "grommet";
 import { useEffect, useState } from "react";
-
-
-
-
+import { baseURL, config } from "../services";
 
 export default function Form() {
   const [value, setValue] = useState("");
   let [cards, setCards] = useState([]);
 
-
   useEffect(() => {
-    getCards();
+    getCardsData();
   }, []);
 
-  async function getCards() {
-    let response = await axios.get(
-      https://api.airtable.com/v0/appWckoNu6Y3xzoi6/Tarot?api_key=key3lFPlQyOhoC8kq
-    );
-    setCards(response.data);
-    console.log(setCards)
+  let cardsDrawn = []
+
+  // async function getPokemon() {
+  //   try {
+  //     console.log("getPokemon");
+  //     for (let i = 1; i <= 6; i++) {
+  //       console.log(`${url}${i}`);
+  //       //make random num
+  //       let randomPkmn = getRandomNumber(151);
+  //       console.log(randomPkmn);
+  //       //api call for pkmn
+  //       const response = await axios.get(`${url}${randomPkmn}`);
+  //       console.log(response.data);
+  //       wildPkmn.push(response.data);
+  //       wildPkmn.push(response.data);
+  //     }
+  //   } catch (err) {
+  //     console.log(err.message);
+  //   }
+  //   console.log(wildPkmn);
+  // }
+
+  async function getCardsData() {
+    try {
+      for (let i = 1; i < 3; i++) {
+        let randomCard = getRandomNumber(22);
+        let response = await axios.get(baseURL, config, randomCard);
+      setCards(response.data.records);
+      console.log(response);
+      cardsDrawn.push(response.data.records)
+
+      }
+    } catch (err) {
+      console.log(err.message)
+    }
+    console.log(cardsDrawn)
   }
 
-
   return (
-    <Box align="center" justify="center" width="medium">
-      <TextInput
-        placeholder="type here"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      />
-    </Box>
+    <div>
+      <div className="cardGrid">
+        {cards.map((card) => {
+          return <img src={card.fields.image} key={card.id} />;
+        })}
+      </div>
+    </div>
+
+    
+
+    // <Box align="center" justify="center" width="medium">
+    //   <TextInput
+    //     placeholder=""
+    //     value={value}
+    //     onChange={(event) => setValue(event.target.value)}
+    //   />
+    // </Box>
   );
+  function getRandomNumber(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 }
