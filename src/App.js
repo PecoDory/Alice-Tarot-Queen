c983338926ad025a62ef31e756f364d9d35c4a72
Peco Dory <pecodory@Pecos-MBP.attlocal.net>
2021-03-30 00:28:13 -0400

    button brings cards in

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import { useEffect, useState } from "react";
 import { Route, Link, useHistory } from "react-router-dom";
 import Header from "./components/Header";
 import Form from "./components/Form";
-import {Box,Button,Heading,Collapsible,Grommet,ResponsiveContext,Layer,} from "grommet";
+import {
+  Box,
+  Button,
+  Heading,
+  Collapsible,
+  Grommet,
+  ResponsiveContext,
+  Layer,
+  Image,
+} from "grommet";
 import { FormClose, Notification } from "grommet-icons";
 import GrommetExample from "./components/GrommetExample";
+import { baseURL, config } from "./services";
+import axios from "axios";
 
 const theme = {
   global: {
@@ -30,13 +41,92 @@ const theme = {
 };
 
 function App() {
+  let [cards, setCards] = useState([]);
+  let [pastCard, setPastCard] = useState(null);
+  let [presentCard, setPresentCard] = useState(null);
+  let [futureCard, setFutureCard] = useState(null);
+  let [newQuestion, setNewQuestion] = useState(null);
+
+  useEffect(() => {
+    getCardsData();
+  }, []);
+
+  const handleAskQuestion = (question) => {
+    postQuestion(question);
+    pickCards();
+  };
+  const postQuestion = async (question) => {
+    console.log("post question");
+    console.log(question);
+    //const response = await axios.post
+    //make axios .post call
+
+    //response as new question state
+    setNewQuestion(question);
+  };
+
+  const pickCards = () => {
+    const cardDeck = cards;
+
+    //get random index
+
+    //remove card from cardDeck at random index
+
+    setPastCard(cards[0]);
+    setPresentCard(cards[1]);
+    setFutureCard(cards[2]);
+    console.log("pick cards");
+  };
+  
+
+  async function getCardsData() {
+    try {
+      let response = await axios.get(baseURL, config);
+      setCards(response.data.records);
+      console.log(response);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+  console.log(pastCard);
+  console.log(presentCard);
   return (
     <Grommet theme={theme} full>
       <ResponsiveContext.Consumer>
         {(size) => (
           <Box fill>
             <Header />
-            <Form />
+            <Form handleAskQuestion={handleAskQuestion} />
+
+            {pastCard && presentCard && futureCard && (
+              <Box>
+                {/* if this is true if this is true if this is true stop make box (always in brackets conditional rendering) */}
+                <Box className="cardGrid" direction="row" gap="medium">
+                  <Box >
+                    <Image
+                      
+                      fit="cover"
+                      src={pastCard.fields.image}
+                      key={pastCard.id}
+                    />
+                  </Box>
+                  <Box >
+                    <Image
+                      fit="cover"
+                      src={presentCard.fields.image}
+                      key={presentCard.id}
+                    />
+                  </Box>
+                  <Box >
+                    <Image
+                      fit="cover"
+                      src={futureCard.fields.image}
+                      key={futureCard.id}
+                    />
+                  </Box>
+                </Box>
+              </Box>
+            )}
 
             {/* <Route path="/list" </Route> */}
             {/* <GrommetExample /> */}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,76 +1,30 @@
 import React from "react";
 import axios from "axios";
 import "./Form.css";
-import { TextInput, Box } from "grommet";
+import { TextInput, Box, Button } from "grommet";
 import { useEffect, useState } from "react";
 import { baseURL, config } from "../services";
 
-export default function Form() {
+
+
+export default function Form({handleAskQuestion}) {
   const [value, setValue] = useState("");
-  let [cards, setCards] = useState([]);
-
-  useEffect(() => {
-    getCardsData();
-  }, []);
-
-  let cardsDrawn = []
-
-  // async function getPokemon() {
-  //   try {
-  //     console.log("getPokemon");
-  //     for (let i = 1; i <= 6; i++) {
-  //       console.log(`${url}${i}`);
-  //       //make random num
-  //       let randomPkmn = getRandomNumber(151);
-  //       console.log(randomPkmn);
-  //       //api call for pkmn
-  //       const response = await axios.get(`${url}${randomPkmn}`);
-  //       console.log(response.data);
-  //       wildPkmn.push(response.data);
-  //       wildPkmn.push(response.data);
-  //     }
-  //   } catch (err) {
-  //     console.log(err.message);
-  //   }
-  //   console.log(wildPkmn);
-  // }
-
-  async function getCardsData() {
-    try {
-      for (let i = 1; i < 3; i++) {
-        let randomCard = getRandomNumber(22);
-        let response = await axios.get(baseURL, config, randomCard);
-      setCards(response.data.records);
-      console.log(response);
-      cardsDrawn.push(response.data.records)
-
-      }
-    } catch (err) {
-      console.log(err.message)
-    }
-    console.log(cardsDrawn)
-  }
+  
+  
+  
 
   return (
-    <div>
-      <div className="cardGrid">
-        {cards.map((card) => {
-          return <img src={card.fields.image} key={card.id} />;
-        })}
-      </div>
-    </div>
-
     
-
-    // <Box align="center" justify="center" width="medium">
-    //   <TextInput
-    //     placeholder=""
-    //     value={value}
-    //     onChange={(event) => setValue(event.target.value)}
-    //   />
-    // </Box>
+    <Box align="center" justify="center" width="medium">
+      <TextInput
+        placeholder=""
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <Button onClick={(e) => handleAskQuestion(value)} primary label="label" />
+      
+    </Box>
+    
   );
-  function getRandomNumber(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+  
 }
